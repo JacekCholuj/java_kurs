99551bbabfed345325a75e5b73162c16e54d062f
Choluj Jacek <rb9310G@raiffeisen.pl>
2016-07-26 16:38:13 +0200

    Correct MainCompoundInterest

--- lesson4/src/main/java/pl/raiffeisensolutions/MainCompoundInterest.java
+++ lesson4/src/main/java/pl/raiffeisensolutions/MainCompoundInterest.java
@@ -1,15 +1,36 @@
 package pl.raiffeisensolutions;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 /**
  * Created by rb9310 on 2016-07-26.
  */
 public class MainCompoundInterest {
     public static void main(String[] args) {
-        BigDecimal principalSum = "3400.00"; //p
-        BigDecimal nominalInterestRate = "4.00";//r
-        BigDecimal compoundingFrequency = "2.00";//n
-        BigDecimal numberOfYear = "3.00"; //t
+        BigDecimal principalSum = new BigDecimal("3400.00"); //p
+        BigDecimal nominalInterestRate = new BigDecimal("4.00");//i
+        BigDecimal compoundingFrequency = new BigDecimal("2.00");//n
+        BigDecimal numberOfYear = new BigDecimal("3.00"); //t
+        BigDecimal wynik = new BigDecimal("0.00");
+        BigDecimal potega = new BigDecimal("0.00");
+
+        CompoundInterest compoundInterest = new CompoundInterest();
+        compoundInterest.setPrincipalSum(principalSum);
+        compoundInterest.setNominalInterestRate(nominalInterestRate);
+        compoundInterest.setCompoundingFrequency(compoundingFrequency);
+        compoundInterest.setNumberOfYear(numberOfYear);
+        System.out.println(compoundInterest);
+
+
+        potega = compoundingFrequency.multiply(numberOfYear);
+        wynik = (BigDecimal.ONE.add(nominalInterestRate.divide(compoundingFrequency,2))).pow(potega.intValue()).setScale(0,RoundingMode.DOWN);
+        System.out.println(wynik.multiply(principalSum));
+
+
+
+
+
     }
 }
