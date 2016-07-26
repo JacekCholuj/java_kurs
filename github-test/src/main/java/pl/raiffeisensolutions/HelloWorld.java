5f25fd4e2f113534c8bb536bf0c9988b8e2b6a0c
Choluj Jacek <rb9310G@raiffeisen.pl>
2016-07-26 14:39:46 +0200

    Add github-test

---
+++ github-test/src/main/java/pl/raiffeisensolutions/HelloWorld.java
@@ -0,0 +1,10 @@
+package pl.raiffeisensolutions;
+
+/**
+ * Created by rb9310 on 2016-07-26.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
